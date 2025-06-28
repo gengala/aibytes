be88561643935b32530f948c92d78ce638231506
Gala <g.gala@tue.nl>
2025-06-28 18:10:27 +0200

    rope class

--- rope.py
+++ rope.py
@@ -1,25 +1,36 @@
 import torch
 
-def apply_rope(x: torch.Tensor, base: float = 10_000.0):
-    """ applies eq. 34 in https://arxiv.org/abs/2104.09864 to every head """
 
-    batch, seq_len, num_heads, head_dim = x.shape
-    assert head_dim % 2 == 0, "head_dim must be even for RoPE"
-    inv_freq = 1.0 / (base ** (torch.arange(0, head_dim, 2, device=x.device).float() / head_dim))
-    seq_idx = torch.arange(seq_len, device=x.device).float()
-    angles = seq_idx[:, None] * inv_freq[None, :]
-    sin = angles.sin().repeat_interleave(2, dim=-1).view(1, seq_len, 1, head_dim)  # can be precomputed and cached
-    cos = angles.cos().repeat_interleave(2, dim=-1).view(1, seq_len, 1, head_dim)  # can be precomputed and cached
+class RoPE(torch.nn.Module):
 
-    def rotate_every_two(x):
-        x1 = x[..., ::2]
-        x2 = x[..., 1::2]
-        return torch.stack((-x2, x1), dim=-1).flatten(-2)
+    def __init__(self, dim, base=10_000):
+        super().__init__()
+        self.dim = dim
+        self.inv_freq = 1.0 / (base ** (torch.arange(0, self.dim, 2).float() / self.dim))
+        self.seq_len_cached = None
+        self.cos_cached = None
+        self.sin_cached = None
 
-    return x * cos + rotate_every_two(x) * sin
+    def forward(self, x):
+        self._build_sin_cos(x)
+        x1 = x[..., :self.dim // 2]
+        x2 = x[..., self.dim // 2:]
+        y1 = x1 * self.cos_cached + x2 * self.sin_cached
+        y2 = x2 * self.cos_cached - x1 * self.sin_cached
+        return torch.cat([y1, y2], 3)
+
+    def _build_sin_cos(self, x):
+        seq_len = x.size(1)
+        if seq_len != self.seq_len_cached:
+            self.seq_len_cached = seq_len
+            seq_idx = torch.arange(seq_len, device=x.device).type_as(self.inv_freq)
+            angles = torch.outer(seq_idx, self.inv_freq).to(x.device)
+            self.cos_cached = angles.cos()[None, :, None, :]
+            self.sin_cached = angles.sin()[None, :, None, :]
 
 
-batch, seq_len, num_heads, head_dim = 2, 128, 12, 64
+batch, seq_len, num_heads, head_dim = 2, 128, 12, 6
 x = torch.randn(batch, seq_len, num_heads, head_dim)
-x_rope = apply_rope(x)
-print(x.shape, x_rope.shape)  # both should be (2, 128, 12, 64)
+rope = RoPE(head_dim)
+x_rope = rope(x)
+print(x.shape, x_rope.shape)
